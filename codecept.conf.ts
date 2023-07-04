0b153cfb23fb27e95578615905fbb8715cc0730e
Selma Brimah <132901736+Selmabugbuster@users.noreply.github.com>
2023-07-04 13:18:21 +0200

    Update codecept.conf.ts

--- codecept.conf.ts
+++ codecept.conf.ts
@@ -14,9 +14,9 @@ export const config: CodeceptJS.MainConfig = {
   output: './output',
   helpers: {
     REST: {
-      endpoint: 'https://charles-qa.hello-charles.com/api/v0',
+      endpoint: 'BaseURl',
       onRequest: (request) => {
-        request.headers['X-API-Key'] = '885d5c64-4586-4d0c-a3d1-61ca21f551d5';
+        request.headers['X-API-Key'] = '';
       },
     },
     JSONResponse: {},
